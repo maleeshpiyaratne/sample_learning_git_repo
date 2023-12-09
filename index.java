03f8faa11e926588645786ec792399bd05bd3893
Maleesh Piyaratne <maleeshpiyaratne@gmail.com>
2023-12-09 09:38:05 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,30 @@
+public class index {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        interview_question_01("Maleesh");
+
+        int [] num = [2, 5, 3, 7, 4, 8, 7];
+        interview_question_02(number_list);
+
+    }
+
+    // 1. Can you write a method to print a String in reverse?
+    public static void interview_question_01(String name){
+
+        for(int i =name.length()-1; i >= 0; i--){
+            System.out.print(name.charAt(i));
+        }
+
+    }
+
+    // 2. Can you write a method that can find the integer that is duplicated in an array
+    // Array = [2, 5, 3, 7, 4, 8, 7]
+    public static void interview_question_02(int [] number_list){
+
+        
+
+    }
+
+}
